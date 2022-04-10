7fd68891f70282ae1d88c15b557dea4e8e1a90fe
Angel Pena Ruiz <angel@cultureamp.com>
2022-04-11 09:38:35 +1000

    reduce complexity

--- lib/three_letters.rb
+++ lib/three_letters.rb
@@ -7,6 +7,10 @@ class ThreeLetters
   def build_tree(a, b)
     return [""] if a == 0 && b == 0
 
+    process_a(a, b) + process_b(a, b)
+  end
+
+  def process_a(a, b)
     results = []
 
     if a > 0
@@ -14,6 +18,12 @@ class ThreeLetters
         results.push(subresult.prepend("a")) unless subresult.start_with?("aa")
       end
     end
+
+    results
+  end
+
+  def process_b(a, b)
+    results = []
 
     if b > 0
       build_tree(a, b - 1).each do |subresult|
