18d7233687cdcf7fd869afb5fd0adc97aa88c3ba
Angel Pena Ruiz <angel@cultureamp.com>
2022-04-11 12:57:14 +1000

    optimise exercise three

--- lib/exercise_three.rb
+++ lib/exercise_three.rb
@@ -9,24 +9,36 @@
 
 class ExerciseThree
   def solution(a)
-    target_emisions = a.inject(&:+) / 2
-    sorted_factories = a.reject { |emission| emission == 0 }.sort.reverse
-    current_emissions = sorted_factories.clone
+    current_emissions = a.reject { |emission| emission == 0 }.clone
+    target_emisions = current_emissions.inject(&:+) / 2
 
     total_filters = 0
-    current_factory = 0
 
     while current_emissions.inject(&:+) > target_emisions
-      while current_emissions.inject(&:+) > target_emisions &&
-            (current_factory == sorted_factories.size - 1 || current_emissions[current_factory] >= current_emissions[current_factory + 1])
-
-        current_emissions[current_factory] = current_emissions[current_factory] / 2
-        total_filters += 1
-      end
-
-      current_factory += 1
+      current_emissions, partial_filters = process_factories(current_emissions.sort.reverse, target_emisions)
+      total_filters += partial_filters
     end
 
     total_filters
+  end
+
+  private
+
+  def process_factories(current_emissions, target_emisions)
+    current_factory = 0
+    partial_filters = 0
+
+    while current_emissions.inject(&:+) > target_emisions
+      current_emissions[current_factory] = current_emissions[current_factory] / 2
+      partial_filters += 1
+
+      break if current_factory == current_emissions.size - 1 # done with this pass
+
+      if current_emissions[current_factory] <= current_emissions[current_factory + 1] # move on if done with factory
+        current_factory += 1
+      end
+    end
+
+    [current_emissions, partial_filters] # be functional! :)
   end
 end
--- spec/exercise_three_spec.rb
+++ spec/exercise_three_spec.rb
@@ -2,8 +2,12 @@ require "exercise_three"
 
 describe ExerciseThree do
   it "case" do
+    # correctness tests
     expect(described_class.new.solution([5, 19, 8, 1])).to eq(3)
     expect(described_class.new.solution([10, 10])).to eq(2)
     expect(described_class.new.solution([3, 0, 5])).to eq(2)
+
+    # performanace tests
+    expect(described_class.new.solution(1000.times.map { |_| Random.rand(500) })).to be > 0
   end
 end
